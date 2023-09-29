55784a906879721b9de8a024e2b41f130c140b43
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-09-29 20:22:45 -0300

    feat new resolve

--- src/layout/DarkModeBg.tsx
+++ src/layout/DarkModeBg.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDarkMode } from "../context/DarkModeProvider";
-import './DarkModeBg.css'
 
 export const DarkModeBg = ({ children }: { children: React.ReactNode }) => {
   const { darkMode } = useDarkMode();
 
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+      
+    }
+
+    return () => {
+      document.body.classList.remove('dark');
+    };
+  }, [darkMode]);
+
   return (
-    <div className={`${darkMode ? "dark" : ""}`}>
+    <>
       <div className={`flex flex-col`}>
         {children}
       </div>
-    </div>
+    </>
   );
 };
