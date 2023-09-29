8a1532a4bdc61e9bc92f5a0e9dec38eb35692b15
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-09-29 00:04:25 -0300

    url fixing

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -15,7 +15,7 @@ export const Content: React.FC = () => {
 
   useEffect(() => {
     const getDataCountries = async () => {
-      const url = "/data.json";
+      const url = "/dist/data.json";
       try {
         setLoading(true);
         const res = await fetch(url);
