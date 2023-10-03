1b73bd2f7dcd0236f559bc1d700aecbaaf6ee7d3
GonzaloArray <gonzalo.arrayaran@gmail.com>
2023-10-03 17:09:57 -0300

    feat: new resposinve details

--- src/page/Details.tsx
+++ src/page/Details.tsx
@@ -16,7 +16,7 @@ export const Details: React.FC = () => {
   const country = CountriesFlag.find((country) => country.cioc === id);
 
   return (
-    <div className="mt-10 h-screen animate__animated animate__fadeIn">
+    <div className="mt-10 h-screen animate__animated animate__fadeIn px-6 md:px-0">
       <div className="inline-block">
         <Link
           to="/"
@@ -33,13 +33,13 @@ export const Details: React.FC = () => {
       </div>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
         <div className="flex flex-col justify-between gap-7">
-          <img src={country?.flags.png} alt={country?.name} className="w-4/5" />
+          <img src={country?.flags.png} alt={country?.name} className="w-full md:w-4/5" />
         </div>
         <div
           className={`flex flex-col gap-4 ${darkMode ? "text-lime-50" : ""}`}
         >
           <h2 className="text-2xl font-bold">{country?.name}</h2>
-          <div className="grid grid-cols-2 gap-6 mt-2 ">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mt-2 ">
             <div className="flex flex-col gap-2">
               <p className="text-sm font-bold">
                 Native Name:{" "}
@@ -82,7 +82,7 @@ export const Details: React.FC = () => {
                 </span>
               </h2>
             </div>
-            <div className="flex items-center gap-3 col-span-2">
+            <div className="flex flex-col md:flex-row md:items-center gap-3 md:col-span-2 pb-10 md:pb-0">
               <h2 className="text-sm font-bold">Border Countries: </h2>
               <div className="flex flex-wrap gap-3">
                 {country?.borders?.map((border) => (
